502aa44d3416a52d7cdb9a74866f9a3c11f957de
BoomZa Nightcore <krittameth193@gmail.com>
2021-03-10 12:31:11 +0700

    sorry here is .cs file Mr.senpai XD

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace workshophomework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            switch (mode)
+            {
+                case "time": 
+                    if (x >= 0)
+                    {
+                        Console.WriteLine(Math.Pow(x - 1, 2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid mouse position");
+                    }
+                    break;
+                case "price":
+                    if (x >= 0)
+                    {
+                        Console.WriteLine(Math.Sqrt(y) + 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid mouse position");
+                    } 
+                    break;
+                default:
+                    Console.WriteLine("Invalid mode");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
